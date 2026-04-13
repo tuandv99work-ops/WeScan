ae1aa5c2ec28f37b87eb54dff4f413b03d9a7a8b
tuandv99work-ops <tuandv99.work@gmail.com>
2026-04-13 13:58:34 +0700

    fix: remove commented-out @available annotations on legacy properties

--- WeScan/ImageScannerController.swift
+++ WeScan/ImageScannerController.swift
@@ -194,16 +194,16 @@ public struct ImageScannerResults {
     /// The detected rectangle which was used to generate the `scannedImage`.
     public var detectedRectangle: Quadrilateral
     
-//    @available(*, unavailable, renamed: "originalScan")
+    
     public var originalImage: UIImage?
     
-//    @available(*, unavailable, renamed: "croppedScan")
+    
     public var scannedImage: UIImage?
     
-//    @available(*, unavailable, renamed: "enhancedScan")
+    
     public var enhancedImage: UIImage?
     
-//    @available(*, unavailable, renamed: "doesUserPreferEnhancedScan")
+    
     public var doesUserPreferEnhancedImage: Bool = false
     
     init(detectedRectangle: Quadrilateral, originalScan: ImageScannerScan, croppedScan: ImageScannerScan, enhancedScan: ImageScannerScan?, doesUserPreferEnhancedScan: Bool = false) {
